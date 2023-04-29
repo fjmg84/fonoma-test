ecf1d34ae08c5acc3c247acdffef79fc12ae4743
fjmg84 <fjmgqba@gmail.com>
2023-04-29 13:56:38 -0400

    create styles-component

---
+++ styled/Div.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ContainerForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5%;
+`;
+
+export const ContainerData = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+  width: 400px;
+  height: 100%;
+  min-height: 300px;
+  max-height: 400px;
+  background-color: rgb(212 212 212);
+  border-radius: 10px;
+  border: 4px solid white;
+`;
