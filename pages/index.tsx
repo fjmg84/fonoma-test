fd8225d8972a6ae0b7262ae9151c7a6e6c402518
fjmg84 <fjmgqba@gmail.com>
2023-04-30 03:32:47 -0400

    add react-query and implement cache

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,41 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Select from "react-select";
+import { QueryClient, useMutation, useQuery } from "react-query";
 
-import { getCurrentSymbol } from "@/utils/services";
-import { useGetConvert } from "@/hooks/useGetConvert";
+import { getConvert, getCurrentSymbol } from "@/utils/services";
 import { Button } from "@/styled/Button";
 import { Input } from "@/styled/Input";
 
 import currentSymbols from "../data/currentSymbols.json";
 import { ContainerData, ContainerForm } from "@/styled/Div";
 import styles from "@/styles/Home.module.css";
+import { ValuesQuery } from "@/interfaces";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ listSymbols }: any) {
   const [options] = useState(listSymbols);
-  const { isError, isLoading, values, handleRequest } = useGetConvert();
+  const prevValues = useRef<ValuesQuery>();
 
-  console.log({ isError, isLoading, values });
+  const { mutate, isLoading, isError, data: values } = useMutation(getConvert);
 
   const handleSubmit = async (e: FormEvent<HTMLElement>) => {
     e.preventDefault();
     const { target } = e;
 
     let data: any = Object.fromEntries(new FormData(target as HTMLFormElement));
-    handleRequest(data);
+    if (
+      prevValues.current?.amount === data.amount &&
+      prevValues.current?.from === data.from &&
+      prevValues.current?.to === data.to
+    )
+      return;
+    else {
+      prevValues.current = { ...data };
+      mutate(data);
+    }
   };
 
   return (
@@ -65,7 +75,7 @@ export default function Home({ listSymbols }: any) {
         </form>
         <div className="container">
           {isLoading && <div className="loading" />}
-          {isError.state && <h4 className="error">{isError.message}</h4>}
+          {isError && <h4 className="error">{isError}</h4>}
           {!isLoading && values?.success && (
             <ContainerData>
               <h4 className="amount">
